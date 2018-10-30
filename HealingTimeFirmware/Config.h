93f8db88f22a01407992c6f5e6bb2f3ea9dc34da
Matthew Gates <matthewg42@gmail.com>
2018-10-30 20:20:38 +0000

    Final

--- HealingTimeFirmware/Config.h
+++ HealingTimeFirmware/Config.h
@@ -4,10 +4,10 @@
 #include <Arduino.h>
 
 // Day/night times
-//const uint32_t WakeSeconds                  = (8  * 3600L); // 08:00 - first trigger *Proper*
-//const uint32_t SleepSeconds                 = (19 * 3600L); // 19:00 - last trigger *Proper*
-const uint32_t WakeSeconds                  = (0  * 3600L); // 08:00 - first trigger
-const uint32_t SleepSeconds                 = (24 * 3600L); // 19:00 - last trigger
+//const uint32_t WakeSeconds                  = (8  * 3600L);  // 08:00 - first trigger *Proper*
+//const uint32_t SleepSeconds                 = (19 * 3600L);  // 19:00 - last trigger *Proper*
+const uint32_t WakeSeconds                  = (7.75  * 3600L); // 07:45 - first trigger
+const uint32_t SleepSeconds                 = (19.75 * 3600L); // 19:45 - last trigger
 
 // Stepper parameters
 const uint16_t StepperNormalSpeed           = 300;    // *Proper* values (30 second rotation)
@@ -17,8 +17,8 @@ const uint16_t StepperNormalAcceleration    = 100;
 
 //const uint32_t Period1                      = 450;  // 450 seconds = 7.5 mins *Proper*
 //const uint32_t Period2                      = 1800; // 1800 seconds = 30 mins *Proper*
-const uint32_t Period1                      = 36;  // 450 seconds = 7.5 mins *Testing*
-const uint32_t Period2                      = 36; // 1800 seconds = 30 mins *Testing*
+const uint32_t Period1                      = 450;  // 450 seconds = 7.5 mins *Testing*
+const uint32_t Period2                      = 1800; // 1800 seconds = 30 mins *Testing*
 
 /////////////////////////////////////////////////////////////////////////////////
 // Don't modify stuff below here if you want to stay sane.
@@ -30,8 +30,8 @@ const int32_t StartupFudge                  = 1000;
 
 // The serial speed. Faster = less jerk, but potentially less reliable in
 // electrically noisy environments.
-//const uint32_t SerialBaud                   = 9600;
-const uint32_t SerialBaud                   = 115200;
+const uint32_t SerialBaud                   = 9600;
+//const uint32_t SerialBaud                   = 115200;
 
 // The address of the clock device (from DS3231.cpp)
 const int RtcAddress                        = 0x68;
